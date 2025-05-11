df6b9549dc0c894851b1b6ba04ddd7f64f9c9d23
NOTTI <wtj4615@gmail.com>
2025-05-11 18:43:13 +0100

    Update whitelisted.lua

--- whitelisted.lua
+++ whitelisted.lua
@@ -6,7 +6,7 @@ Format: [UserID] = {Rank = Premium/Admin, Logo = AssetID},
 
 return {
   [8448763249] = {Rank = "Admin", Logo = "rbxassetid://108769013608592"},
-  [8274679019] = {Rank = "Admin", Logo = "rbxassetid://108769013608592"}, 
+  [8454953765] = {Rank = "Admin", Logo = "rbxassetid://108769013608592"}, 
   [4136435201] = {Rank = "Admin", Logo = "rbxassetid://108769013608592"},
   [7981371006] = {Rank = "Premium", Logo = "rbxassetid://131565214240422"},
   [7678440425] = {Rank = "Premium", Logo = "rbxassetid://131565214240422"},
