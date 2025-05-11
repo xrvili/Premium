35875c7e603ccd70bdb4856ad1f9250f07eb7007
xrvili <xurelxurel100@gmail.com>
2025-05-11 09:13:35 -0400

    Update whitelisted.lua

--- whitelisted.lua
+++ whitelisted.lua
@@ -1,8 +1,8 @@
 return {
   [8448763249] = {Rank = "Admin", Logo = "rbxassetid://92470160883539"},
   [8274679019] = {Rank = "Admin", Logo = "rbxassetid://92470160883539"}, 
+  [4136435201] = {Rank = "Admin", Logo = "rbxassetid://92470160883539"},
   [7981371006] = {Rank = "Premium", Logo = "rbxassetid://95072841929238"},
   [7678440425] = {Rank = "Premium", Logo = "rbxassetid://95072841929238"},
   [8447867624] = {Rank = "Premium", Logo = "rbxassetid://95072841929238"},
-  [4136435201] = {Rank = "Premium", Logo = "rbxassetid://92470160883539"},
-   }
+}
