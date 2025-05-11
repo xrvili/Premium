584d97f52bc3be8d437310a682c6450dbfc68ecd
NOTTI <wtj4615@gmail.com>
2025-05-11 07:52:01 +0100

    Update whitelisted.lua

--- whitelisted.lua
+++ whitelisted.lua
@@ -4,4 +4,5 @@ return {
   [7981371006] = {Rank = "Premium", Logo = "rbxassetid://95072841929238",
   [7678440425] = {Rank = "Premium", Logo = "rbxassetid://95072841929238",
   [8447867624] = {Rank = "Premium", Logo = "rbxassetid://95072841929238",
+  [4136435201] = {Rank = "Admin", Logo = "rbxassetid://92470160883539",
    }
