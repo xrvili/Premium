57259fbc235ae881130efc62d3c3e20433b25297
xrvili <xurelxurel100@gmail.com>
2025-05-11 15:57:24 -0400

    Update whitelisted.lua

--- whitelisted.lua
+++ whitelisted.lua
@@ -6,7 +6,7 @@ Format: [UserID] = {Rank = Premium/Admin, Logo = AssetID},
 
 return {
   [2239776711] = {Rank = "Admin", Logo = "rbxassetid://108769013608592"},
-  [8448763249] = {Rank = "Admin", Logo = "rbxassetid://108769013608592"},
+  [8448763249] = {Rank = "Premium", Logo = "rbxassetid://108769013608592"},
   [8454953765] = {Rank = "Admin", Logo = "rbxassetid://108769013608592"}, 
   [4136435201] = {Rank = "Admin", Logo = "rbxassetid://108769013608592"},
   [7981371006] = {Rank = "Premium", Logo = "rbxassetid://131565214240422"},
