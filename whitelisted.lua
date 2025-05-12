8272d215200d4afda3b9de30e0f81b13a223c37b
NOTTI <wtj4615@gmail.com>
2025-05-12 18:40:39 +0100

    Update whitelisted.lua

--- whitelisted.lua
+++ whitelisted.lua
@@ -14,5 +14,6 @@ return {
   [8447867624] = {Rank = "Premium", Logo = "rbxassetid://131565214240422"},
   [1596548284] = {Rank = "Premium", Logo = "rbxassetid://131565214240422"},   --- changed rbx id due to account ban for 1day
   [121340313] = {Rank = "Premium", Logo = "rbxassetid://131565214240422"},
-  [8443279457] = {Rank = "Preium", Logo = "rbxassetid://131565214240422"},   --- Remove if the 2 boosts go.
+  [8443279457] = {Rank = "Premium", Logo = "rbxassetid://131565214240422"},   --- Remove if the 2 boosts go.
+  [8274679019] = {Rank = "Premium", Logo = "rbxassetid://108769013608592"},   --- NOTTI account
 }
