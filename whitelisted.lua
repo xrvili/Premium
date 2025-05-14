617e84971828371b99dc7f4637c4a9d90e734bb4
NOTTI <wtj4615@gmail.com>
2025-05-14 18:03:12 +0100

    Update whitelisted.lua

--- whitelisted.lua
+++ whitelisted.lua
@@ -15,5 +15,6 @@ return {
   [1596548284] = {Rank = "Premium", Logo = "rbxassetid://131565214240422"},   --- changed rbx id due to account ban for 1day. Discord ID: 774320685760774154 (@fearxvgod)
   [121340313] = {Rank = "Premium", Logo = "rbxassetid://131565214240422"},
   [8443279457] = {Rank = "Premium", Logo = "rbxassetid://131565214240422"},   --- Remove if the 2 boosts go. Discord ID: 1219667692353618050 (@ineedbalencis)
-  [8461091289] = {Rank = "Admin", Logo = "rbxassetid://108769013608592"},   
+  [8461091289] = {Rank = "Admin", Logo = "rbxassetid://108769013608592"},  
+  [8470406766] = {Rank = "Premium", Logo = "rbxassetid://131565214240422"},
 }
