962499e3c0adfc9813c2cd1b5b34a083af0a9661
raquis2223 <dawgmaliek@gmail.com>
2025-05-15 20:15:02 -0400

    Update whitelisted.lua

--- whitelisted.lua
+++ whitelisted.lua
@@ -11,10 +11,11 @@ return {
   [4136435201] = {Rank = "Admin", Logo = "rbxassetid://108769013608592"},
   [7981371006] = {Rank = "Premium", Logo = "rbxassetid://131565214240422"},
   [7678440425] = {Rank = "Premium", Logo = "rbxassetid://131565214240422"},
-  [8447867624] = {Rank = "Premium", Logo = "rbxassetid://131565214240422"},
   [1596548284] = {Rank = "Premium", Logo = "rbxassetid://131565214240422"},   --- changed rbx id due to account ban for 1day. Discord ID: 774320685760774154 (@fearxvgod)
   [121340313] = {Rank = "Premium", Logo = "rbxassetid://131565214240422"},
   [8443279457] = {Rank = "Premium", Logo = "rbxassetid://131565214240422"},   --- Remove if the 2 boosts go. Discord ID: 1219667692353618050 (@ineedbalencis)
   [8461091289] = {Rank = "Admin", Logo = "rbxassetid://108769013608592"},  
   [8470406766] = {Rank = "Premium", Logo = "rbxassetid://131565214240422"},
+  [8472532762] = {Rank = "Premium", Logo = "rbxassetid://131565214240422"},   --- His second WL, Got API banned on his old account. Discord ID: 615046943420186634 (@devdotsix)
+  
 }
