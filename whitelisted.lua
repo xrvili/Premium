5bbb2a14b1aa19e01c68c97f5f205dee44b5d065
xrvili <xurelxurel100@gmail.com>
2025-05-18 01:08:38 -0400

    Update whitelisted.lua

--- whitelisted.lua
+++ whitelisted.lua
@@ -10,7 +10,7 @@ return {
   [8454953765] = {Rank = "Admin", Logo = "rbxassetid://108769013608592"}, 
   [4136435201] = {Rank = "Admin", Logo = "rbxassetid://108769013608592"},
   [7981371006] = {Rank = "Premium", Logo = "rbxassetid://131565214240422"},
-  [7678440425] = {Rank = "Premium", Logo = "rbxassetid://131565214240422"},
+  [7678440425] = {Rank = "Admin", Logo = "rbxassetid://108769013608592"}, -- bought admin 5k robux. Discord ID: 1207774260039524465 (@mr.ekb_87768)
   [1596548284] = {Rank = "Premium", Logo = "rbxassetid://131565214240422"},   --- changed rbx id due to account ban for 1day. Discord ID: 774320685760774154 (@fearxvgod)
   [121340313] = {Rank = "Premium", Logo = "rbxassetid://131565214240422"},
   [8443279457] = {Rank = "Premium", Logo = "rbxassetid://131565214240422"},   --- Remove if the 2 boosts go. Discord ID: 1219667692353618050 (@ineedbalencis)
