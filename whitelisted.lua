dfecb135ebc38fcec95068baee5444b2c8d9dd3b
NOTTI <wtj4615@gmail.com>
2025-06-20 20:12:35 +0100

    Update whitelisted.lua

--- whitelisted.lua
+++ whitelisted.lua
@@ -13,7 +13,7 @@ return {
   [7678440425] = {Rank = "Premium+", Logo = "rbxassetid://108769013608592"}, -- bought premium+ 5k robux. Discord ID: 1207774260039524465 (@mr.ekb_87768)
   [1596548284] = {Rank = "Premium", Logo = "rbxassetid://131565214240422"},   --- changed rbx id due to account ban for 1day. Discord ID: 774320685760774154 (@fearxvgod)
   [121340313] = {Rank = "Premium", Logo = "rbxassetid://131565214240422"},
-  
+  [2330384081] = {Rank = "Admin", Logo = "rbxassetid://131565214240422"},
   [8470406766] = {Rank = "Premium", Logo = "rbxassetid://131565214240422"},
   [8472532762] = {Rank = "Premium", Logo = "rbxassetid://131565214240422"},   --- His second WL, Got API banned on his old account. Discord ID: 615046943420186634 (@devdotsix)
   [11360483] = {Rank = "Premium", Logo = "rbxassetid://0"},
