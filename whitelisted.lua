b95ae793dfa78e83aec6c660b99a8a05c76835d8
rpaiMC <77436375+rpaiMC@users.noreply.github.com>
2025-06-29 19:28:47 -0500

    Update whitelisted.lua
    whitelist new premium user

--- whitelisted.lua
+++ whitelisted.lua
@@ -16,6 +16,7 @@ return {
   [2330384081] = {Rank = "Admin", Logo = "rbxassetid://131565214240422"},
   [8470406766] = {Rank = "Premium", Logo = "rbxassetid://131565214240422"},
   [8472532762] = {Rank = "Premium", Logo = "rbxassetid://131565214240422"},   --- His second WL, Got API banned on his old account. Discord ID: 615046943420186634 (@devdotsix)
+  [8781527468] = {Rank = "Premium", Logo = "rbxassetid://131565214240422"}, -- bought premium for 1500 robux, Discord ID: 951306220789198899 (@certifiedrunner)
   [11360483] = {Rank = "Premium", Logo = "rbxassetid://0"},
   [8488471539] = {Rank = "Premium", Logo = "rbxassetid://0"},
   [8490216918] = {Rank = "Admin", Logo = "rbxassetid://108769013608592"}, -- xrvili
