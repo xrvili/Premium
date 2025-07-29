d5508571bd9cfcc4ef72af08d940e830222aa19d
xrvili <xurelxurel100@gmail.com>
2025-07-28 20:28:58 -0400

    Update whitelisted.lua

--- whitelisted.lua
+++ whitelisted.lua
@@ -5,4 +5,15 @@ Format: [UserID] = {Rank = Premium/Admin, Logo = AssetID},
 ]]
 
 return {
-  [2239776711] = {Rank = "Admin", Logo = "rbxassetid://108769013608592"},
+  [8448763249] = {Rank = "Premium", Logo = "rbxassetid://108769013608592"},
+  [7981371006] = {Rank = "Premium", Logo = "rbxassetid://131565214240422"},
+  [7678440425] = {Rank = "Premium+", Logo = "rbxassetid://108769013608592"}, -- bought premium+ 5k robux. Discord ID: 1207774260039524465 (@mr.ekb_87768)
+  [1596548284] = {Rank = "Premium", Logo = "rbxassetid://131565214240422"},   --- changed rbx id due to account ban for 1day. Discord ID: 774320685760774154 (@fearxvgod)
+  [121340313] = {Rank = "Premium", Logo = "rbxassetid://131565214240422"},
+  [8470406766] = {Rank = "Premium", Logo = "rbxassetid://131565214240422"},
+  [8472532762] = {Rank = "Premium", Logo = "rbxassetid://131565214240422"},   --- His second WL, Got API banned on his old account. Discord ID: 615046943420186634 (@devdotsix)
+  [8781527468] = {Rank = "Premium", Logo = "rbxassetid://131565214240422"}, -- bought premium for 1500 robux, Discord ID: 951306220789198899 (@certifiedrunner)
+  [11360483] = {Rank = "Premium", Logo = "rbxassetid://0"},
+  [8488471539] = {Rank = "Premium", Logo = "rbxassetid://0"},
+  [8490216918] = {Rank = "Admin", Logo = "rbxassetid://108769013608592"}, -- xrvili
+}
